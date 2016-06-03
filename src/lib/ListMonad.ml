157f03b18775a78e79c5567236b326efb662f2f2
Matthew Chan <matt@themattchan.com>
2016-06-02 18:47:17 -0700

    why why why

--- src/lib/ListMonad.ml
+++ src/lib/ListMonad.ml
@@ -11,12 +11,8 @@ module M : (Monad.S with type 'a m := 'a list) =
 module A = Applicative.FromMonad(M)
 module F = Functor.FromApplicative(A)
 
-open M
-open A
-open F
-
 let cart_prod xs ys =
-  L.(xs >>= (fun x ->
-    ys >>= (fun y ->
-            return (x,y))))
+  M.(xs >>= (fun x ->
+       ys >>= (fun y ->
+               return (x,y))))
 ;;
